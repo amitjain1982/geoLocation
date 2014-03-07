7f260e63a51d7e532373e5fe04f8f5de0535bda2
Amit Jain <amitjain1982@gmail.com>
2014-03-07 21:16:34 +0530

    Return distance from each hub

--- grails-app/controllers/com/intelligrape/geolocation/GeolocationController.groovy
+++ grails-app/controllers/com/intelligrape/geolocation/GeolocationController.groovy
@@ -7,9 +7,9 @@ class GeolocationController {
         // Test URL : https://maps.googleapis.com/maps/api/geocode/json?address=60,+Ansari+roa,+Darya+Ganj,+Delhi,+India&sensor=false&key=AIzaSyBMxEnf4R54RtQErWdz3Qq3ajDhsr2Nl3s
         List<GeoDistance> locations =[]
         locations <<  new GeoDistance(1,28.6405645,77.24177259999999)// DelhiGate
-        locations << new GeoDistance(3,77.3740732,28.5621974)//Sector-49 Noida
-        locations << new GeoDistance(2,77.23189339999999,28.6505535) //ChandniChowk
-        locations << new GeoDistance(4,77.46575469999999,28.4446394) // JayPee Aman Sec -151 Noida
+        locations << new GeoDistance(3,28.5621974,77.3740732)//Sector-49 Noida
+        locations << new GeoDistance(2,28.6505535, 77.23189339999999) //ChandniChowk
+        locations << new GeoDistance(4,28.4446394,77.46575469999999) // JayPee Aman Sec -151 Noida
 
 //        GeoAddressDTO dto = new GeoAddressDTO(addressLine1: 'Sab mall, sector-18', city: 'Noida', state: 'uttar pradesh', country: 'India')
         GeoAddressDTO dto = new GeoAddressDTO(addressLine1: 'golcha', city: 'new delhi', state: 'Delhi', country: 'India')
@@ -18,7 +18,7 @@ class GeolocationController {
 
         try {
             List nearestLocations = geoLocationService.listNearestLocationInOrder(dto,locations)
-            render nearestLocations*.hubId
+            render nearestLocations.collect{["Hub : " + it.hubId +" - distance " + it.distance]}
         } catch (RuntimeException e) {
             render e.message
         }
